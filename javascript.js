c0472d851f00aeb77c48f3b05f4e1411408b38dd
kmj611 <kmj060101@gmail.com>
2022-07-12 16:57:08 +0900

    검색기능 추가

--- javascript.js
+++ javascript.js
@@ -1,2 +1,7 @@
-// var searchtext;
+var searchtext;
 // `https://www.youtube.com/results?search_query=${searchtext}`
+
+function searchf() {
+    const searchText = document.getElementById('검색').value;
+    location.href=`https://www.youtube.com/results?search_query=${searchText}`;
+}
