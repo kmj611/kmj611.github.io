24d0dacf093d409b0a14d833d42b0d157bf27747
kmj611 <kmj060101@gmail.com>
2022-07-12 18:07:15 +0900

    거의 완성

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-var searchtext;
-// `https://www.youtube.com/results?search_query=${searchtext}`
-
 function searchf() {
     const searchText = document.getElementById('검색').value;
     location.href=`https://www.youtube.com/results?search_query=${searchText}`;
